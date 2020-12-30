89448e122a3e002e3cc0efc617ea5050911a3728
sandipan898 <sandipan.das898@gmail.com>
2020-12-30 21:23:38 +0530

    adding class based views

--- MyProject/api/urls.py
+++ MyProject/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
-from .views import article_list, article_detail
+from .views import article_list, article_detail, ArticleAPIView, ArticleDetailsView
 
 urlpatterns = [
-    path('articles/', article_list, name="article-list"),
-    path('detail/<int:pk>', article_detail, name="article-detail"),
+    # path('articles/', article_list, name="article-list"),
+    path('articles/', ArticleAPIView.as_view(), name='article-list'),
+    # path('detail/<int:pk>', article_detail, name="article-detail"),
+    path('details/<int:id>/', ArticleDetailsView.as_view(), name="article-detail")
 ]
--- MyProject/api/views.py
+++ MyProject/api/views.py
@@ -10,7 +10,66 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
+from rest_framework.views import APIView
+
+from rest_framework import generics
+from rest_framework import mixins
+
 # Create your views here.
+
+
+""""""""""""""""""""" Class Based Views """""""""""""""""""""
+
+
+class ArticleAPIView(APIView):
+
+    def get(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True) # serializing all articles into objects
+        return Response(serializer.data) # takes serialized data and sends response in JSON format
+
+    def post(self, request):
+        serializer = ArticleSerializer(data=request.data) # serializing in comming request data 
+
+        if serializer.is_valid(): # checking validity
+            serializer.save() # saving the serialized data into model instance
+            return Response(serializer.data, status=status.HTTP_201_CREATED) # takes serialized data and sends response in JSON format
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) # returning error 
+
+
+class ArticleDetailsView(APIView):
+
+    def get_object(self, id):
+        try:
+            return Article.objects.get(id=id) # trying to find the article object matching the 'id' passed
+        except Article.DoesNotExist:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, id):
+        """ GET Method to Get or Read the article from database """ 
+        article = self.get_object(id)
+        serializer = ArticleSerializer(article) # serializing the article object found 
+        return Response(serializer.data) # returning serialized data into JSON format
+
+    def put(self, request, id):
+        """ PUT Method to Update the Article """
+        article = self.get_object(id)        
+        serializer = ArticleSerializer(article, data=request.data) # serializing the article object with the parsed data 
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        article = self.get_object(id)        
+        article.delete() # deleting the article object found
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
+
+
+""""""""""""""""""""" Function Based Views """""""""""""""""""""
+
 
 # @csrf_exempt #allows post requests without csrf token
 @api_view(['GET', 'POST']) # decorating views with django-rest-framework api views to add special functionalities
@@ -59,3 +118,4 @@ def article_detail(request, pk):
         article.delete() # deleting the article object found
         return Response(status=status.HTTP_204_NO_CONTENT)
         
+
