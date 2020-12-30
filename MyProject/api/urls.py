74aa133afbe8547bc4028f830c66fca2f1b711f4
sandipan898 <sandipan.das898@gmail.com>
2020-12-30 23:03:26 +0530

    adding generic views and authentications

--- MyProject/api/urls.py
+++ MyProject/api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
-from .views import article_list, article_detail, ArticleAPIView, ArticleDetailsView
+from .views import article_list, article_detail, ArticleAPIView, ArticleDetailsView, GenericArticleView
 
 urlpatterns = [
     # path('articles/', article_list, name="article-list"),
     path('articles/', ArticleAPIView.as_view(), name='article-list'),
+    path('generic/articles/', GenericArticleView.as_view(), name='article-list'),
+    path('generic/articles/<int:id>/', GenericArticleView.as_view(), name='article-list'),
+    
     # path('detail/<int:pk>', article_detail, name="article-detail"),
     path('details/<int:id>/', ArticleDetailsView.as_view(), name="article-detail")
 ]
